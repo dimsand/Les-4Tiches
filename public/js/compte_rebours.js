4485265ff79f521a5a036352f9ad9ea615b698eb
Dimitri <dimitri.sandron@outlook.fr>
2018-03-29 21:58:19 +0200

    first commit

---
+++ public/js/compte_rebours.js
@@ -0,0 +1,64 @@
+function initCompteRebours(conteneur)
+{
+    var compteRebours = conteneur.find('.compte_rebours'),
+        jours = compteRebours.find('.jours'),
+        heures = compteRebours.find('.heures'),
+        minutes = compteRebours.find('.minutes'),
+        secondes = compteRebours.find('.secondes'),
+        lanceCompteRebours,
+        boucleCompteur,
+        formateNombre,
+        dateFinTeasing;
+
+
+    $.getJSON( "settings.json", function( data ) {
+        console.log(data);
+        dateFinTeasing = new Date(data.dateDepartCOurse);
+        var items = [];
+        $.each( data, function( key, val ) {
+            items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+    });
+
+    // Ajoute un zéro si le nombre est inférieur à 10
+    formateNombre = function(n)
+    {
+        return (n < 10 ? '0' : '') + n;
+    };
+
+    lanceCompteRebours = function()
+    {
+        console.log(dateFinTeasing);
+        var maintenant = new Date(),
+            // le temps restant en secondes
+            tempsRestant = (dateFinTeasing.getTime() - maintenant.getTime()) / 1000,
+            j, h, m;
+
+        // Une fois que le compte à rebours est terminé
+        if(tempsRestant <= 0)
+        {
+            clearInterval(boucleCompteur);
+            return;
+        }
+
+        // nombre de jours restants
+        j = Math.floor(tempsRestant / 86400);
+        tempsRestant -= j * 86400;
+
+        // nombre d'heures restantes
+        h = Math.floor(tempsRestant / 3600);
+        tempsRestant -= h * 3600;
+
+        // nombre de minutes restantes
+        m = Math.floor(tempsRestant / 60);
+        tempsRestant -= m * 60;
+
+        jours.text(formateNombre(j));
+        heures.text(formateNombre(h));
+        minutes.text(formateNombre(m));
+        secondes.text(formateNombre(Math.floor(tempsRestant)));
+    };
+
+    // On appelle la fonction toutes les secondes pour obtenir le compte à rebours
+    boucleCompteur = setInterval(lanceCompteRebours, 1000);
+}
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    {{--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116543519-1"></script>--}}
+    {{--<script>--}}
+        {{--window.dataLayer = window.dataLayer || [];--}}
+        {{--function gtag(){dataLayer.push(arguments);}--}}
+        {{--gtag('js', new Date());--}}
+        {{--gtag('config', 'UA-116543519-1');--}}
+    {{--</script>--}}
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Nous participerons, sous le nom d'équipage Les 4Tiches, le Raid 4L Trophy, du 21 avril au 3 mars 2019">
+    <meta name="author" content="Dimitri Sandron">
+    <meta name="keywords" content="les 4tiches, 4tiches, les fortiches, fortiches, 4ltrophy, 4L Trophy, Raid humanitaire, course, Maroc" />
+    <meta name="robots" content="index,follow" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Les 4Tiches - @yield('title')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/landing-page.min.css" rel="stylesheet">
+    <link href="css/parallax.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="./">4L Trophy</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="la-team.html">La team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown07">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+            <div class="my-2 my-md-0 text-light text-uppercase compte_rebours">
+                <i class="fa fa-flag-checkered mr-2" data-toggle="tooltip" data-placement="bottom" title="Départ de la course dans"></i>
+                <span class="badge badge-light jours" data-toggle="tooltip" data-placement="bottom" title="Jours">XX</span> : <!-- JOURS -->
+                <span class="badge badge-light heures" data-toggle="tooltip" data-placement="bottom" title="Heures">XX</span> : <!-- HEURES -->
+                <span class="badge badge-light minutes" data-toggle="tooltip" data-placement="bottom" title="Minutes">XX</span> : <!-- MINUTES -->
+                <span class="badge badge-light secondes" data-toggle="tooltip" data-placement="bottom" title="Secondes">XX</span> <!-- SECONDES -->
+            </div>
+        </div>
+    </div>
+</nav>
+
+@yield('content')
+
+<!-- Footer -->
+<footer class="footer bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                <ul class="list-inline mb-2">
+                    <li class="list-inline-item">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Contact</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Terms of Use</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                </ul>
+                <p class="text-muted small mb-4 mb-lg-0">&copy; Les 4Tiches 2018. All Rights Reserved.</p>
+            </div>
+            <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fa fa-facebook fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fa fa-twitter fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fa fa-instagram fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="js/jquery/jquery.min.js"></script>
+<script src="js/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="js/compte_rebours.js"></script>
+
+<script>
+    initCompteRebours($("#navbarsExample07"));
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
+
+</body>
+
+</html>
