bb75e1b098167e196d7a871fee7465b122af369e
Dimitri <dimitri.sandron@outlook.fr>
2018-04-08 21:55:02 +0200

    add formulaire helloasso financement

--- resources/views/financement.blade.php
+++ resources/views/financement.blade.php
@@ -13,9 +13,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cupiditate deserunt est et laborum officiis quam quasi quia suscipit veritatis! Beatae dolorum maxime nesciunt nulla perferendis recusandae, reiciendis temporibus. Tenetur.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cupiditate deserunt est et laborum officiis quam quasi quia suscipit veritatis! Beatae dolorum maxime nesciunt nulla perferendis recusandae, reiciendis temporibus. Tenetur.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cupiditate deserunt est et laborum officiis quam quasi quia suscipit veritatis! Beatae dolorum maxime nesciunt nulla perferendis recusandae, reiciendis temporibus. Tenetur.</p>
+                <iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/les-4tiches/collectes/participation-au-raid-4l-trophy/widget" style="width:100%;height:750px;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe><div style="width:100%;text-align:center;">Propulsé par <a href="https://www.helloasso.com" rel="nofollow">HelloAsso</a></div>
             </div>
         </div>
     </div>
