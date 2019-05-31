0802296f286dcb64fe5af5020db4cda45e4c4086
Dimitri <dimitri.sandron@outlook.fr>
2019-05-31 21:31:42 +0200

    add button lien photo sur page accueil + changement photo page accueil + photos equipage dans page equipage + photo mwith more button in gallery on photos tab

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -3,8 +3,6 @@
 @section('title', 'Photos')
 
 @section('stylesheet')
-    <link rel="stylesheet" type="text/css" media="all" href="css/jgallery/font-awesome.min.css"/>
-    <link rel="stylesheet" type="text/css" media="all" href="css/jgallery/jgallery.min.css?v=1.6.4"/>
     <link href="https://unpkg.com/nanogallery2@2.4.2/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
     <style>
         span.jgallery-btn.slideshow, span.jgallery-btn.full-screen, span.jgallery-btn.minimalize-thumbnails {
@@ -16,15 +14,13 @@
 @section('content')
 
     <div class="jumbotron paral paralsec_photos mt-5">
-        <h1 class="display-3">Les photos</h1>
+        <h1 class="display-3">Les photos de l'aventure</h1>
     </div>
 
-    <section class="banner-sec photos">
+    <section class="banner-sec photos mb-3 mt-2">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 mt-2">
-                {{--<div id="nanogallery2">gallery_made_with_nanogallery2</div>--}}
-
 
                 <!-- ### gallery content ### -->
                     <div class="card ">
@@ -48,6 +44,8 @@
     "thumbnailHeight":  150,
     "thumbnailWidth":   "auto",
     "thumbnailHoverEffect2":   "scale120",
+    "galleryDisplayMode": "moreButton",
+    "galleryDisplayMoreStep": 3,
     "itemsBaseURL":     "{{ env('APP_URL') }}/{{ env('GALLERIES_FOLDER') }}"
   }'>
                                                 @foreach($sousGallery as $photo)
@@ -63,54 +61,6 @@
                         </div>
                     </div>
 
-                    {{--@foreach($galleries as $labelGallery => $gallery)--}}
-                        {{--<h2>{{ $labelGallery }}</h2>--}}
-                        {{--@foreach($gallery as $labelSousGallery => $sousGallery)--}}
-                            {{--<h3>{{ $labelSousGallery }}</h3>--}}
-                            {{--<div data-nanogallery2='{--}}
-    {{--<!-- ### gallery settings ### -->--}}
-    {{--"thumbnailHeight":  150,--}}
-    {{--"thumbnailWidth":   150,--}}
-    {{--"itemsBaseURL":     "{{ env('APP_URL') }}/{{ env('GALLERIES_FOLDER') }}"--}}
-  {{--}'>--}}
-                                {{--@foreach($sousGallery as $photo)--}}
-                                    {{--<a href="{{ env('APP_URL') }}/{{ env('GALLERIES_FOLDER') }}/{{ $photo }}"--}}
-                                       {{--data-ngthumb="{{ env('APP_URL') }}/{{ env('GALLERIES_FOLDER') }}/{{ $photo }}">Title--}}
-                                        {{--Image 1</a>--}}
-                                {{--@endforeach--}}
-                            {{--</div>--}}
-                        {{--@endforeach--}}
-                    {{--@endforeach--}}
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="banner-sec photos">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 mt-2">
-                    <div id="gallery">
-                        <div class="album" data-jgallery-album-title="Notre 4L">
-                            <a href="{{ "/img/albums/4l/4l_1.jpg" }}"><img src="{{ "/img/albums/4l/4l_1.jpg" }}"
-                                                                           alt="Notre 4L dans un si beau paysage..."/></a>
-                            <a href="{{ "/img/albums/4l/4l_2.jpg" }}"><img src="{{ "/img/albums/4l/4l_2.jpg" }}"
-                                                                           alt="Notre 4L à l'ombre"/></a>
-                            <a href="{{ "/img/albums/4l/4l_3.jpg" }}"><img src="{{ "/img/albums/4l/4l_3.jpg" }}"
-                                                                           alt="Première inspection mécanique de la 4L"/></a>
-                            <a href="{{ "/img/albums/4l/4l_4.jpg" }}"><img src="{{ "/img/albums/4l/4l_4.jpg" }}"
-                                                                           alt="Notre 4L en plein cagnard ! Heureusement qu'il y a le toît ouvrant ;)"/></a>
-                        </div>
-                        <div class="album" data-jgallery-album-title="L'équipage">
-                            <a href="{{ "/img/albums/equipage/equipage_1.jpg" }}"><img
-                                        src="{{ "/img/albums/equipage/equipage_1.jpg" }}"
-                                        alt="Nous voici, nous voilà !"/></a>
-                            <a href="{{ "/img/albums/equipage/equipage_2.jpg" }}"><img
-                                        src="{{ "/img/albums/equipage/equipage_2.jpg" }}"
-                                        alt="11 ans d'amitié, pour 10 jours d'aventure extraordinaire !"/></a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -119,48 +69,15 @@
 @endsection
 
 @section('javascript')
-    <script type="text/javascript" src="js/jgallery/jgallery.min.js?v=1.6.4"></script>
-    <script>
-        $(function () {
-            $("#gallery").jGallery({
-                "transition": "fade_moveFromRight",
-                "transitionBackward": "fade_moveFromLeft",
-                "transitionCols": "1",
-                "transitionRows": "1",
-                "thumbnailsPosition": "bottom",
-                "thumbType": "image",
-                "backgroundColor": "#ffffff",
-                "textColor": "#000000",
-                "mode": "standard",
-                tooltips: false,
-                titleExpanded: false
-            });
-        });
-    </script>
-
     <!-- nanogallery2 -->
     <script type="text/javascript" src="https://unpkg.com/nanogallery2@2.4.2/dist/jquery.nanogallery2.min.js"></script>
     <script>
-        //        jQuery(document).ready(function () {
-        //
-        //            jQuery("#nanogallery2").nanogallery2( {
-        //                // ### gallery settings ###
-        //                thumbnailHeight:  150,
-        //                thumbnailWidth:   150,
-        //                itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
-        //
-        //                // ### gallery content ###
-        //                items: [
-        //                    { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-        //                    { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-        //                    { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
-        //                ]
-        //            });
-        //        });
 
-        $("section.photos").on('click', '.nav-link', function(e) {
-            $(".ngy2_container").nanogallery2('refresh');
+        $( document ).ready(function() {
+            $('#myTab li:first a').trigger('click');
+            $("section.photos").on('click', '.nav-link', function(e) {
+                $(".ngy2_container").nanogallery2('refresh');
+            });
         });
-
     </script>
 @endsection
